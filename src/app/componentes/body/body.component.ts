431674b9d0f9f1537f3415ea07073cb0989f7b35
JPerez1005 <perezbuenojuliandavid@gmail.com>
2024-04-26 16:27:31 -0500

    "implementacion de adminlte en angular"

--- src/app/componentes/body/body.component.ts
+++ src/app/componentes/body/body.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
+import { SidebarLeftComponent } from "../sidebar-left/sidebar-left.component";
+import { ContenidoPrincipalComponent } from "../contenido-principal/contenido-principal.component";
+import { FooterComponent } from "../footer/footer.component";
+import { SidebarRightComponent } from "../sidebar-right/sidebar-right.component";
 
 @Component({
-  selector: 'app-body',
-  standalone: true,
-  imports: [],
-  templateUrl: './body.component.html',
-  styleUrl: './body.component.css'
+    selector: 'app-body',
+    standalone: true,
+    templateUrl: './body.component.html',
+    styleUrl: './body.component.css',
+    imports: [NavbarComponent, SidebarLeftComponent, ContenidoPrincipalComponent, FooterComponent, SidebarRightComponent]
 })
 export class BodyComponent {
 
